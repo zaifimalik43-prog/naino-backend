30ae80cd207415c67f5a2498f117cf7249c9bfe0
Muhammad Huzaifa <zaifimalik43@gmail.com>
2026-06-05 15:02:36 +0500

    fix: allow localhost:5174 in CORS

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "http://localhost:5174",
         "https://naino-frontend.vercel.app"
     ],
     allow_credentials=True,
